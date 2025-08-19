e132bf006041b0ac83f62afe60445d9a6bb7eb28
sahithiperumalla <109752174+sahithiperumalla@users.noreply.github.com>
2025-08-19 08:45:36 +0530

    Add files via upload

---
+++ student/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Student from './components/Student';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Student Details</h1>
+      <Student />
+    </div>
+  );
+}
+
+export default App;
